d5962d398eeb8d29784fdd6aa22cf4b4c7d784cd
xerxesv <xerxesv@gmail.com>
2015-12-31 19:33:13 +0000

    generate regions for snapping clothing

--- client/js/models/DollModel.js
+++ client/js/models/DollModel.js
@@ -3,8 +3,8 @@ var DollModel = Backbone.Model.extend({
     baseSrc: '_default_man1.png',
     clothing: {},
     regions: {
-      coats: [ [0,82,228,178] ], // [topleftX, topleftY, regionwidth, regionheight]
-      pants: [ [48,211,79,53] ],
+      coats: [ [0,82,228,178] ], // each array has format [topleftX, topleftY, regionwidth, regionheight]
+      pants: [ [48,211,143,247] ],
       tails: [ [0,230,228,270] ]
     }
   }
--- client/js/views/DollView.js
+++ client/js/views/DollView.js
@@ -9,15 +9,28 @@ var DollView = Backbone.View.extend({
   render: function () {
     var img = new Image();
     var $dollDiv = this.$el.children('.doll');
-
-    console.log('dolldiv found? ', $dollDiv);
-
     img.src = './img/' + this.model.get('baseSrc');
     img.addEventListener('load', function (e) {
       $dollDiv.css('width', e.target.width + 'px');
       $dollDiv.css('height', e.target.height + 'px');
     })
     $dollDiv.css('background-image', 'url("./img/' + this.model.get('baseSrc') + '")');
+
+    //generate div regions on the doll, for snapping
+    _.each(this.model.get('regions'), function (regions, name) {
+      // regions is an array of 4-element arrays
+      // name is the object key
+      _.each(regions, function (region) {
+        var $div = $('<div></div>');
+        $div.addClass('region');
+        $div.addClass(name);
+        $div.css('left', region[0] + 'px');
+        $div.css('top', region[1] + 'px');
+        $div.css('width', region[2] + 'px');
+        $div.css('height', region[3] + 'px');
+        $dollDiv.append($div);
+      })
+    })
 
     return this.$el;
   }
